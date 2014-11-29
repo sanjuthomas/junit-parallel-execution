c3f5a72e945206b63ffcb597d33052a58897f96f
sanjuthomas <sanju@sanju.org>
2014-11-29 18:30:03 -0500

    adding testcase

--- junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest1.java
+++ junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest1.java
@@ -1,5 +1,10 @@
 package com.ourownjava.junit;
 
+import static org.junit.Assert.assertEquals;
+import mockit.Tested;
+
+import org.junit.Test;
+
 /**
  * 
  * @author Sanju Thomas
@@ -7,7 +12,21 @@ package com.ourownjava.junit;
  */
 public class TestUnitUnderTest1 {
 	
+	@Tested
 	private UnitUnderTest1 unitUnderTest1;
 	
+	@Test
+	public void shouldReturnMethod1() throws InterruptedException{
+		assertEquals("method1", unitUnderTest1.method1());
+	}
 	
+	@Test
+	public void shouldReturnMethod2() throws InterruptedException{
+		assertEquals("method2", unitUnderTest1.method2());
+	}
+	
+	@Test
+	public void shouldReturnMethod3() throws InterruptedException{
+		assertEquals("method3", unitUnderTest1.method3());
+	}
 }
--- junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest2.java
+++ junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest2.java
@@ -1,10 +1,33 @@
 package com.ourownjava.junit;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import mockit.Tested;
+
 /**
  * 
  * @author Sanju Thomas
  *
  */
 public class TestUnitUnderTest2 {
+	
+	@Tested
+	private UnitUnderTest2 unitUnderTest2;
 
+	@Test
+	public void shouldReturnMethod1() throws InterruptedException{
+		assertEquals("method1", unitUnderTest2.method1());
+	}
+	
+	@Test
+	public void shouldReturnMethod2() throws InterruptedException{
+		assertEquals("method2", unitUnderTest2.method2());
+	}
+	
+	@Test
+	public void shouldReturnMethod3() throws InterruptedException{
+		assertEquals("method3", unitUnderTest2.method3());
+	}
 }
