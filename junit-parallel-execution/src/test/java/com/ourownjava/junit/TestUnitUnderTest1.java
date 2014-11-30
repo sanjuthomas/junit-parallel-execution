5b0f858e6dc138a6957a1f756e775f1e3b401477
sanjuthomas <sanju@sanju.org>
2014-11-29 19:07:12 -0500

    adding maven plugin for parallel execution

--- junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest1.java
+++ junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest1.java
@@ -2,6 +2,8 @@ package com.ourownjava.junit;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,16 +23,19 @@ public class TestUnitUnderTest1 {
 	
 	@Test
 	public void shouldReturnMethod1() throws InterruptedException{
+		System.out.println("TestUnitUnderTest1.shouldReturnMethod1()" + new Date());
 		assertEquals("method1", unitUnderTest1.method1());
 	}
 	
 	@Test
 	public void shouldReturnMethod2() throws InterruptedException{
+		System.out.println("TestUnitUnderTest1.shouldReturnMethod2()" + new Date());
 		assertEquals("method2", unitUnderTest1.method2());
 	}
 	
 	@Test
 	public void shouldReturnMethod3() throws InterruptedException{
+		System.out.println("TestUnitUnderTest1.shouldReturnMethod3()" + new Date());
 		assertEquals("method3", unitUnderTest1.method3());
 	}
 }
--- junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest2.java
+++ junit-parallel-execution/src/test/java/com/ourownjava/junit/TestUnitUnderTest2.java
@@ -2,6 +2,8 @@ package com.ourownjava.junit;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,16 +26,19 @@ public class TestUnitUnderTest2 {
 
 	@Test
 	public void shouldReturnMethod1() throws InterruptedException{
+		System.out.println("TestUnitUnderTest2.shouldReturnMethod1()" + new Date());
 		assertEquals("method1", unitUnderTest2.method1());
 	}
 	
 	@Test
 	public void shouldReturnMethod2() throws InterruptedException{
+		System.out.println("TestUnitUnderTest2.shouldReturnMethod2()" + new Date());
 		assertEquals("method2", unitUnderTest2.method2());
 	}
 	
 	@Test
 	public void shouldReturnMethod3() throws InterruptedException{
+		System.out.println("TestUnitUnderTest2.shouldReturnMethod3()" + new Date());
 		assertEquals("method3", unitUnderTest2.method3());
 	}
 }
